e73372fea1c522488c137dc20243790dc2d3807c
justinmmorgan21 <justinmorgan21@gmail.com>
2024-11-03 21:33:10 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { TripsPage } from "./TripsPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <TripsPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/TripsPage.jsx
@@ -0,0 +1,7 @@
+export function TripsPage() {
+  return (
+    <main>
+      <h1>Welcome to React!</h1>
+    </main>
+  )
+}
