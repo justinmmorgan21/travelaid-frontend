68e623840295707ec9d5d7681b4d391f25738e1c
justinmmorgan21 <justinmorgan21@gmail.com>
2024-11-04 20:42:56 -0600

    install cloudinary

--- src/TripsPage.jsx
+++ src/TripsPage.jsx
@@ -1,19 +1,19 @@
-import { useNavigate } from "react-router-dom"
+// import { useNavigate } from "react-router-dom"
 import { LoginPage } from "./LoginPage";
 export function TripsPage() {
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const handleGoToIndex = () => {
-    console.log("handleGoToIndex");
-    navigate(`/trips`);
-  }
+  // const handleGoToIndex = () => {
+  //   console.log("handleGoToIndex");
+  //   navigate(`/trips`);
+  // }
 
   return (
     <main>
       <h1>Welcome to TravelAde!</h1>
       <LoginPage />
-      <button onClick={()=>handleGoToIndex()}>Go to Trips</button>
+      {/* <button onClick={()=>handleGoToIndex()}>Go to Trips</button> */}
     </main>
   )
 }
