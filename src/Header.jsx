02dc3e3a933c57978a910f75361ecefbe024c270
justinmmorgan21 <justinmorgan21@gmail.com>
2024-11-04 21:52:08 -0600

    added current user to header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,57 @@
 import { Link } from "react-router-dom";
 import { LogoutLink } from "./LogoutLink";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 export function Header() {
+
+  // FROM blog-frontend
+  const [currentUser, setCurrentUser] = useState({});
+
+  const loadUserData = () => {
+    axios.get("http://localhost:3000/users/current.json").then(response=> {
+      console.log(response.data);
+      setCurrentUser(response.data);
+    })
+  }
+
+  useEffect(loadUserData, []);
+
+  let authLinks;
+  let welcomeMessage;
+  if (localStorage.jwt === undefined) {
+    console.log("logged out")
+    authLinks = (
+      <>
+      <li>
+        <Link to="signup">Sign up</Link>
+      </li>
+      <li>
+        <Link to="login">Log in</Link>
+      </li>
+      </>
+    )
+    welcomeMessage = <></>
+  } else {
+    console.log("logged in")
+    authLinks = (
+                <li>
+                  <LogoutLink />
+                </li>
+    )
+    welcomeMessage = (
+      <>
+      Welcome, {currentUser.name}
+      </>
+    )
+  }
+
   return (
     <header>
       <nav>
         <Link to="/">Home</Link> | <Link to="/trips">My Trips</Link> | <Link to="/signup">Signup</Link> | <Link to="Login">Login</Link> | <LogoutLink />
+        {/* {authLinks} */}
+        {welcomeMessage}
+        <img id="user-image" src={currentUser.image_url} alt="" />
       </nav>
     </header>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {AdvancedImage} from '@cloudinary/react';
+import {Cloudinary} from "@cloudinary/url-gen";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
