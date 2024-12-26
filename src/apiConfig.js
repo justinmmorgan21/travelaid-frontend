e719073f2900249b1644726a1a3bcc2b81021223
justinmmorgan21 <justinmorgan21@gmail.com>
2024-12-26 13:42:29 -0600

    set both production and development to access onrender sources rather than local

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -2,11 +2,13 @@ const apiConfig = {
   proxyServerUrl:
     import.meta.env.MODE === "production"
       ? "https://travelaid-proxy.onrender.com"
-      : "http://localhost:3001",
+      : "https://travelaid-proxy.onrender.com",
+      // : "http://localhost:3001",
   backendBaseUrl:
     import.meta.env.MODE === "production"
       ? "https://travelaid-api.onrender.com"
-      : "http://localhost:3000",
+      : "https://travelaid-api.onrender.com",
+      // : "http://localhost:3000",
 };
 
 export default apiConfig;
