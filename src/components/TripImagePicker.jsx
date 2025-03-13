1445a96622879b2e69be53bd43a983b923d265e7
justinmmorgan21 <justinmorgan21@gmail.com>
2025-03-12 20:23:12 -0500

    added routes for openai image generation and image upload to aws S3 bucket to server.cjs

--- src/components/TripImagePicker.jsx
+++ src/components/TripImagePicker.jsx
@@ -3,34 +3,35 @@ import { useState, useEffect } from 'react';
 import { Button, Label, TextInput } from "flowbite-react";
 import apiConfig from '../apiConfig';
 
+
 export function TripImagePicker({onClose, onResult, title}) {
+
   const [userImageUrl, setUserImageUrl] = useState('');
   const [imageUrl, setImageUrl] = useState('');
   const [imageIndex, setImageIndex] = useState(0);
 
-  useEffect(()=>{getImages()}, []);
+  useEffect(()=>{findGoogleImage()}, []);
 
   const [openAiImageUrl, setOpenAiImageUrl] = useState("");
   const [googleImageLoading, setGoogleImageLoading] = useState(false);
   const [openAILoading, setOpenAILoading] = useState(false);
-  const generateImage = () => {
+
+  const generateImage = async () => {
     setOpenAILoading(true);
     try {
-      axios.post("https://api.openai.com/v1/images/generations",
-      {
-        model: "dall-e-3",
-        prompt: `Create an image of a logo for ${title} use the text exactly like: "${title}"`,
-        n: 1, // Number of images
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`, // Secure API key
-        },
-      }).then(response => {
-        setOpenAiImageUrl(response.data.data[0].url);
+        const generateImageResponse = await axios.post("http://localhost:3001/generate-image", {
+            title: title,
+        });
+        const base64String = generateImageResponse.data.base64Image;
+        const formattedBase64 = `data:image/png;base64,${base64String}`;
+          
+        const uploadResponse = await axios.post("http://localhost:3001/upload-image", {
+            imageBinary: formattedBase64,
+            fileName: `user-${Date.now()}.png`,
+        });
+    
+        setOpenAiImageUrl(uploadResponse.data.url);
         setOpenAILoading(false);
-      })
     } catch (error) {
       console.error("Error generating image:", error);
       setOpenAILoading(false);
@@ -42,15 +43,10 @@ export function TripImagePicker({onClose, onResult, title}) {
     const query = title;
     axios.get(`${apiConfig.proxyServerUrl}/get-image`, { params: {query} }).then(response => {
       const image_url = response.data.images_results[imageIndex].thumbnail;
-      setImageUrl(image_url)
+      setImageUrl(image_url);
+      setGoogleImageLoading(false);
     })
     setImageIndex(imageIndex + 1);
-    setGoogleImageLoading(false);
-  }
-
-  const getImages = () => {
-    generateImage();
-    findGoogleImage();
   }
 
   return (
@@ -60,21 +56,21 @@ export function TripImagePicker({onClose, onResult, title}) {
       <div className='flex justify-between mb-10'>
         <div className='flex flex-col'>
           {imageUrl && <div className='border-2 border-gray-400 rounded-md shadow-md cursor-pointer'>
-            <a onClick={()=>onResult(imageUrl)} >
+            <div onClick={()=>onResult(imageUrl)} >
               <img src={imageUrl} alt="Results" width="240px"/>
-            </a>
+            </div>
           </div>}
           <Button className="bg-blue-700 w-full flex items-center h-7 rounded-md text-white" onClick={()=>onResult(imageUrl)} disabled={googleImageLoading}>Select</Button>
-          <Button className="bg-blue-700 w-full flex items-center h-7 mt-2 rounded-md text-white" onClick={()=>{findGoogleImage()}} disabled={googleImageLoading}>{googleImageLoading ? "Generating..." : "Regenerate"}</Button>
+          <Button className="bg-blue-700 w-full flex items-center h-7 mt-2 rounded-md text-white" onClick={()=>{findGoogleImage()}} disabled={googleImageLoading}>{googleImageLoading ? "Searching..." : "Next"}</Button>
         </div>
         <div className='flex flex-col'>
           {openAiImageUrl && <div className='border-2 border-gray-400 rounded-md shadow-md cursor-pointer'>
-            <a onClick={()=>onResult(openAiImageUrl)}>
+            <div onClick={()=>onResult(openAiImageUrl)}>
               <img src={openAiImageUrl} alt="Generated" width="240px"/>
-            </a>
+            </div>
           </div>}
           <Button className="bg-blue-700 w-full flex items-center h-7 rounded-md text-white" onClick={()=>onResult(openAiImageUrl)} disabled={openAILoading}>Select</Button>
-          <Button className="bg-blue-700 w-full flex items-center h-7 mt-2 rounded-md text-white" onClick={()=>{generateImage()}} disabled={openAILoading}>{openAILoading ? "Generating..." : "Regenerate"}</Button>
+          <Button className="bg-blue-700 w-full flex items-center h-7 mt-2 rounded-md text-white" onClick={()=>{generateImage()}} disabled={openAILoading}>{openAILoading ? "Generating..." : (openAiImageUrl ? "Generate new AI image" : "Generate AI image")}</Button>
         </div>
         <div className='flex flex-col w-1/4'>
           <p>Add your own image</p>
